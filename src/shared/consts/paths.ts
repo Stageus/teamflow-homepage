65f9552a1e58ab0cf9fde7d38975caee5508d1bc
junik <wkdwnsdlr37@gmail.com>
2024-09-30 14:48:26 +0900

    Docs(consts): pagePaths 지정

---
+++ src/shared/consts/paths.ts
@@ -0,0 +1,8 @@
+export const PATHS = {
+    login: "login",
+    signup: "signup",
+    teamspaceList: "teamspace-list",
+    teamspace: "teamspace",
+    teamspacePublic: "teamspace-public",
+    teamspacePrivate: "teamspace-private",
+}
