05f092d2d4b47f3678887cfd1d2af5665dd064f4
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-09-30 14:51:41 +0900

    페이지별 Layout  설정 (#4)
    * Docs(consts): pagePaths 지정
    
    * Design(Layout): 페이지별 레이아웃 설정

---
+++ src/app/Layout.tsx
@@ -0,0 +1,21 @@
+import { useLocation, Outlet } from 'react-router-dom';
+import './global.css';
+import { PATHS } from '@shared/consts/paths';
+
+export const Layout = () => {
+   const location = useLocation();
+   const authPath = [`/${PATHS.login}`, `/${PATHS.signup}`];
+   const layoutNone = !authPath.includes(location.pathname.toLocaleLowerCase());
+
+   return (
+      <div className="flex h-screen bg-primary-white dark:bg-primary-black">
+         {layoutNone && <aside className="h-full min-w-aside"></aside>}
+         <div className="flex flex-col grow">
+            {layoutNone && <header className="w-full min-h-header"></header>}
+            <main className="grow">
+               <Outlet />
+            </main>
+         </div>
+      </div>
+   );
+};
