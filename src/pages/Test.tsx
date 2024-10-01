3cdb596ccada9e1ea10c992d1969782708c897ff
junik <wkdwnsdlr37@gmail.com>
2024-10-01 13:21:28 +0900

    Design: hover 색상 적용

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -6,7 +6,6 @@ export const Test = () => {
         <div>test페이지</div>
         <h1>반응형 테스트</h1>
         <div><DefaultButton
-            type='active'
             onClick={()=> console.log("실행")}
             text='테스트'
         /></div>
