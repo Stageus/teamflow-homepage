db30fcda400691160e6f6daa6e71596664395c51
junik <wkdwnsdlr37@gmail.com>
2024-10-01 16:01:26 +0900

    Design(Header): DefaultButton Type 적용

--- src/app/header/ui/Header.tsx
+++ src/app/header/ui/Header.tsx
@@ -3,19 +3,16 @@ export const Header = () => {
    return (
       <div className="flex gap-3 px-4 py-2 justify-start">
          <div>
-            <DefaultButton text="TeamSpace" onClick={() => console.log('TeamSpace 선택페이지')} />
+            <DefaultButton type="notSelected" text="TeamSpace" onClick={() => console.log('TeamSpace 선택페이지')} />
          </div>
          <div>
             <DefaultButton type="notSelected" text="알림" onClick={() => console.log('알림 모달창')} />
          </div>
          <div>
-            <DefaultButton type="active" text="테마" onClick={() => console.log('테마변경')} />
+            <DefaultButton type="notSelected" text="테마" onClick={() => console.log('테마변경')} />
          </div>
          <div>
-            <DefaultButton type="disabled" text="로그아웃" onClick={() => console.log('로그아웃 모달창')} />
-         </div>
-         <div>
-            <DefaultButton type="danger" text="로그아웃" onClick={() => console.log('로그아웃 모달창')} />
+            <DefaultButton type="notSelected" text="로그아웃" onClick={() => console.log('로그아웃 모달창')} />
          </div>
       </div>
    );
