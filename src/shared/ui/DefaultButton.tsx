c373d51a34c5858ae3aa90fac6b59d06efeca3b1
junik <wkdwnsdlr37@gmail.com>
2024-10-01 14:00:05 +0900

    Design(DefaultButton): defaultType 추가

--- src/shared/ui/DefaultButton.tsx
+++ src/shared/ui/DefaultButton.tsx
@@ -1,19 +1,23 @@
-import { DefaultButtonProps } from "./types";
+import { DefaultButtonProps } from './types';
 
 export const DefaultButton = (props: DefaultButtonProps) => {
    const { type, onClick, text } = props;
    const styles = {
-    active: "bg-primary-color text-primary-white",
-    disabled: "bg-primary-gray text-primary-white cursor-not-allowed",
-    danger: "bg-danger-color text-primary-white"
+      default: 'text-primary-black bg-primary-white',
+      active: 'bg-primary-color text-primary-white',
+      disabled: 'bg-primary-gray text-primary-white cursor-not-allowed',
+      danger: 'bg-danger-color text-primary-white',
    };
 
    return (
       <>
-         <button 
-         className={`${type ? styles[type] : "text-primary-black bg-primary-white"} w-full px-4 py-2 rounded-lg shadow whitespace-nowrap hover:bg-secondary-color hover:text-primary-white`}
-         onClick={ type !== "disabled" ? onClick : null}
-         >{text}</button>
+         <button
+            className={`${
+               type ? styles[type] : 'text-primary-white bg-primary-gray'
+            } w-full px-4 py-2 rounded-lg shadow whitespace-nowrap hover:bg-secondary-color hover:text-primary-white`}
+            onClick={type !== 'disabled' ? onClick : null}>
+            {text}
+         </button>
       </>
    );
 };
