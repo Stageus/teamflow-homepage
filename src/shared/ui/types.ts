ca09f94100896638809d40401cec93cc85239e2d
junik <wkdwnsdlr37@gmail.com>
2024-10-02 15:04:55 +0900

    Design(DefaultButton): ItemProps 추가

--- src/shared/ui/types.ts
+++ src/shared/ui/types.ts
@@ -1,7 +1,10 @@
+import { ReactNode } from "react";
+
 type DefaultButtonType = 'default' | 'active' | 'disabled' | 'danger' | 'notSelected';
 
 export interface DefaultButtonProps {
    type?: DefaultButtonType;
    text: string;
+   item?: ReactNode; 
    onClick: () => void;
 }
