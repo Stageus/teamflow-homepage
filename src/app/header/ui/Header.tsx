83d6970e1a3bf8295a789c93c75afd76b9287c9e
junik <wkdwnsdlr37@gmail.com>
2024-10-03 16:00:16 +0900

    Feat(Header): theme conText 적용

--- src/app/header/ui/Header.tsx
+++ src/app/header/ui/Header.tsx
@@ -1,5 +1,9 @@
+import { useRoute } from "@shared/hooks";
+import { useDarkTheme } from "@shared/store/darkTheme";
 import { DefaultButton } from '@shared/ui';
 export const Header = () => {
+   const { changeTheme } = useDarkTheme();
+   const { loginRoute } = useRoute();
    return (
       <div className="flex gap-3 px-4 py-2 justify-start">
          <div>
@@ -9,10 +13,10 @@ export const Header = () => {
             <DefaultButton type="notSelected" text="알림" onClick={() => console.log('알림 모달창')} />
          </div>
          <div>
-            <DefaultButton type="notSelected" text="테마" onClick={() => console.log('테마변경')} />
+            <DefaultButton type="notSelected" text="테마" onClick={changeTheme} />
          </div>
          <div>
-            <DefaultButton type="notSelected" text="로그인" onClick={() => console.log('로그아웃 모달창')} />
+            <DefaultButton type="notSelected" text="로그인" onClick={loginRoute} />
          </div>
          <div>
             <DefaultButton type="notSelected" text="로그아웃" onClick={() => console.log('로그아웃 모달창')} />
