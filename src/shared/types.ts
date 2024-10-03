42634d95680267091e9338c2ac9e26d896166c87
junik <wkdwnsdlr37@gmail.com>
2024-10-03 15:24:47 +0900

    Docs(types): TextInput type 추가

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 
+// DefaultButton
 type DefaultButtonType = 'default' | 'active' | 'disabled' | 'danger' | 'notSelected';
 type DefaultButtonProps = {
    type?: DefaultButtonType;
@@ -8,5 +9,19 @@ type DefaultButtonProps = {
    onClick: () => void;
 };
 export type DefaultButtonComponent = (props: DefaultButtonProps) => JSX.Element;
+
+
+// TextInput
+type Props = {
+   title: string,
+   errorText: string,
+   regex: RegExp,
+   placeholder: string,
+   nextName: string,
+   nextCallback: () => void,
+   cancellName: string,
+   cancellCallback: () => void,
+}
+export type TextInputComponent = ( props: Props ) => JSX.Element;
 
 
