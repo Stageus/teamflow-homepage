8e1602fdef7d283e13126745a90cab0471e6355c
junik <wkdwnsdlr37@gmail.com>
2024-10-05 16:07:09 +0900

    Feat(Header): TeamSpaceList페이지 이벤트 등록

--- src/app/header/ui/Header.tsx
+++ src/app/header/ui/Header.tsx
@@ -1,13 +1,15 @@
+import { useLocation } from "react-router-dom";
 import { useRoute } from "@shared/hooks";
 import { useDarkTheme } from "@shared/store/darkTheme";
 import { DefaultButton } from '@shared/ui';
 export const Header = () => {
    const { changeTheme } = useDarkTheme();
-   const { loginRoute } = useRoute();
+   const { loginRoute, teamspaceListRoute } = useRoute();
+   
    return (
       <div className="flex gap-3 px-4 py-2 justify-start">
          <div>
-            <DefaultButton type="notSelected" text="TeamSpace" onClick={() => console.log('TeamSpace 선택페이지')} />
+            <DefaultButton type="notSelected" text="TeamSpace" onClick={teamspaceListRoute} />
          </div>
          <div>
             <DefaultButton type="notSelected" text="알림" onClick={() => console.log('알림 모달창')} />
@@ -20,7 +22,7 @@ export const Header = () => {
          </div>
          <div>
             <DefaultButton type="notSelected" text="로그아웃" onClick={() => console.log('로그아웃 모달창')} />
-         </div>
+         </div>         
       </div>
    );
 };
