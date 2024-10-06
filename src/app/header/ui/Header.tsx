46329b037bbd8902841f955f3bfdd9ae543b7273
junik <wkdwnsdlr37@gmail.com>
2024-10-06 16:45:28 +0900

    Refactor(Header): Alarm & Header useModal적용

--- src/app/header/ui/Header.tsx
+++ src/app/header/ui/Header.tsx
@@ -4,19 +4,22 @@ import { useDarkTheme } from "@shared/store/darkTheme";
 import { useUrlTracking } from "@shared/store/urlTracking";
 import { DefaultButton } from '@shared/ui';
 import { PATHS } from "@shared/consts/paths";
+import { useModalEventDetect } from "@shared/hooks/useModalEventDetect";
 
 export const Header = () => {
    const { theme, changeTheme } = useDarkTheme();
    const { loginRoute, teamspaceListRoute } = useRoute();
    const { getCurrentPathName } = useUrlTracking();
+   const { modalRef, isModalDetect, onClickOpenModal } = useModalEventDetect();
 
    return (
       <div className="flex gap-3 px-4 py-2 justify-start">
          <div>
             <DefaultButton type={getCurrentPathName(PATHS.teamspaceList) ? "active" : "notSelected"} text="TeamSpace" onClick={teamspaceListRoute} />
          </div>
-         <div>
-            <Alarm/>
+         <div ref={modalRef}>
+            <DefaultButton type={ isModalDetect ? "active" : "notSelected"} text="알림" onClick={onClickOpenModal} />
+            {isModalDetect ? <Alarm/> : null}
          </div>
          <div>
             <DefaultButton type="notSelected" text={theme ? "라이트모드" : "다크모드"} onClick={changeTheme} />
