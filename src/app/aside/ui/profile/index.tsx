76434cb3ed2d40bd838ad6c41e072e2ce9457fef
junik <wkdwnsdlr37@gmail.com>
2024-10-08 16:56:49 +0900

    Refactor(profile): 분리 로직 index.ts 적용

--- src/app/aside/ui/profile/index.tsx
+++ src/app/aside/ui/profile/index.tsx
@@ -1,20 +1,11 @@
-import { useRef, useState } from 'react';
 import { BsGearFill, BsXLg } from 'react-icons/bs';
+import { useMoreProfile } from './model/useMoreProfile';
+import { useProfileImg } from './model/useProfileImg';
 import { DefaultButton } from '@shared/ui';
 import testImg from '@shared/assets/고양이.jpeg';
 export const Profile = () => {
-
-   const [moreProfile, setMoreProfile] = useState<boolean>(false);
-   const onClickOpenMoreProfile = () => setMoreProfile(!moreProfile);
-
-   const inputFileRef = useRef(null);
-
-   const onClickFile = () => {
-
-    if(inputFileRef.current){
-        inputFileRef.current.click();
-    }
-   }
+   const { moreProfile, onClickOpenMoreProfile } = useMoreProfile();
+   const { inputFileRef, onClickFile } = useProfileImg();
 
    const moreStyle = {
       show: 'max-h-20 opacity-100 visible duration-300',
@@ -32,23 +23,18 @@ export const Profile = () => {
                   <span className="text-primary-gray">userEmail@gmail.com</span>
                   <span className="text-primary-color">테스트유저 닉네임</span>
                   <div className={`${moreProfile ? moreStyle.show : moreStyle.hidden} flex gap-2`}>
-                    <div><DefaultButton
-                    size='xs'
-                    type='active'
-                    text='이미지변경'
-                    onClick={onClickFile}
-                    /></div>
-                    <div><DefaultButton
-                    size='xs'
-                    type='active'
-                    text='닉네임변경'
-                    onClick={()=> console.log("input창 오픈")}
-                    /></div>
-                    <input
-                    type='file'
-                    ref={inputFileRef}
-                    className='hidden'
-                    />
+                     <div>
+                        <DefaultButton size="xs" type="active" text="이미지변경" onClick={onClickFile} />
+                     </div>
+                     <div>
+                        <DefaultButton
+                           size="xs"
+                           type="active"
+                           text="닉네임변경"
+                           onClick={() => console.log('input창 오픈')}
+                        />
+                     </div>
+                     <input type="file" ref={inputFileRef} className="hidden" />
                   </div>
                </div>
                <div
@@ -57,15 +43,18 @@ export const Profile = () => {
                   {moreProfile ? <BsXLg size={'25'} /> : <BsGearFill size={'25'} />}
                </div>
             </div>
-            <div className={`${moreProfile ? moreStyle.show : moreStyle.hidden} flex gap-2 absolute bottom-0 left-0 right-0 p-2 bg-light-bg2 dark:bg-dark-bg2 translate-y-full transition-all `}>
-                  <div className="grow flex flex-col gap-3 bg-light-bg1 dark:bg-dark-bg1 rounded-lg p-1">
-                     <span className="text-primary-color">나의 TeamSpace</span>
-                     <span className='text-primary-black dark:text-primary-white'>250</span>
-                  </div>
-                  <div className="grow flex flex-col gap-3 bg-light-bg1 dark:bg-dark-bg1 rounded-lg p-1">
-                     <span className="text-primary-color">참여중인 TeamSpace</span>
-                     <span className='text-primary-black dark:text-primary-white'>250</span>
-                  </div>
+            <div
+               className={`${
+                  moreProfile ? moreStyle.show : moreStyle.hidden
+               } flex gap-2 absolute bottom-0 left-0 right-0 p-2 bg-light-bg2 dark:bg-dark-bg2 translate-y-full transition-all `}>
+               <div className="grow flex flex-col gap-3 bg-light-bg1 dark:bg-dark-bg1 rounded-lg p-1">
+                  <span className="text-primary-color">나의 TeamSpace</span>
+                  <span className="text-primary-black dark:text-primary-white">250</span>
+               </div>
+               <div className="grow flex flex-col gap-3 bg-light-bg1 dark:bg-dark-bg1 rounded-lg p-1">
+                  <span className="text-primary-color">참여중인 TeamSpace</span>
+                  <span className="text-primary-black dark:text-primary-white">250</span>
+               </div>
             </div>
          </div>
       </>
