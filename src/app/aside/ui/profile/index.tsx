bb41e8d76992d69d61842658c06e073fe99c0e49
junik <wkdwnsdlr37@gmail.com>
2024-10-08 16:49:10 +0900

    Design(profile): moreStyle  변경 및 적용

--- src/app/aside/ui/profile/index.tsx
+++ src/app/aside/ui/profile/index.tsx
@@ -17,8 +17,8 @@ export const Profile = () => {
    }
 
    const moreStyle = {
-      show: 'translate-y-full opacity-100 visible',
-      hidden: 'translate-y-3/4 opacity-0 invisible',
+      show: 'max-h-20 opacity-100 visible duration-300',
+      hidden: 'max-h-0 opacity-0 invisible duration-300',
    };
 
    return (
@@ -28,16 +28,18 @@ export const Profile = () => {
                <div className="w-[50px] h-[50px] flex justify-center items-center text-primary-gray">
                   <img src={testImg} className="w-full h-full rounded-full"></img>
                </div>
-               <div className="flex justify-between flex-col flex-grow">
+               <div className="flex justify-between flex-col gap-2 flex-grow">
                   <span className="text-primary-gray">userEmail@gmail.com</span>
                   <span className="text-primary-color">테스트유저 닉네임</span>
-                  <div className='flex gap-2'>
+                  <div className={`${moreProfile ? moreStyle.show : moreStyle.hidden} flex gap-2`}>
                     <div><DefaultButton
+                    size='xs'
                     type='active'
                     text='이미지변경'
                     onClick={onClickFile}
                     /></div>
                     <div><DefaultButton
+                    size='xs'
                     type='active'
                     text='닉네임변경'
                     onClick={()=> console.log("input창 오픈")}
@@ -55,7 +57,7 @@ export const Profile = () => {
                   {moreProfile ? <BsXLg size={'25'} /> : <BsGearFill size={'25'} />}
                </div>
             </div>
-            <div className={`${moreProfile ? moreStyle.show : moreStyle.hidden} flex gap-2 absolute top-0 left-0 right-0 p-2 bg-light-bg2 dark:bg-dark-bg2 transition-transform`}>
+            <div className={`${moreProfile ? moreStyle.show : moreStyle.hidden} flex gap-2 absolute bottom-0 left-0 right-0 p-2 bg-light-bg2 dark:bg-dark-bg2 translate-y-full transition-all `}>
                   <div className="grow flex flex-col gap-3 bg-light-bg1 dark:bg-dark-bg1 rounded-lg p-1">
                      <span className="text-primary-color">나의 TeamSpace</span>
                      <span className='text-primary-black dark:text-primary-white'>250</span>
