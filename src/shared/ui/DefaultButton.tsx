d007d465acfb23471f0509b7d7f6484e50e3c99d
junik <wkdwnsdlr37@gmail.com>
2024-10-08 15:51:05 +0900

    Design(DefaultButton): size Props 추가

--- src/shared/ui/DefaultButton.tsx
+++ src/shared/ui/DefaultButton.tsx
@@ -1,7 +1,7 @@
 import { DefaultButtonProps } from './types';
 
 export const DefaultButton = (props: DefaultButtonProps) => {
-   const { type, onClick, text, item } = props;
+   const { size, type, onClick, text, item } = props;
    const typeStyles = {
       default: 'text-primary-black bg-primary-white hover:bg-secondary-color hover:text-primary-white',
       notSelected: 'text-primary-white bg-primary-gray hover:bg-secondary-color',
@@ -10,13 +10,19 @@ export const DefaultButton = (props: DefaultButtonProps) => {
       danger: 'bg-danger-color text-primary-white hover:bg-secondary-color',
    };
 
+   const sizeStyle = {
+      xs: 'px-2 py-1 text-size-xs',
+      sm: 'px-2 py-1 text-size-sm',
+      default: 'px-4 py-2 text-size-body',
+   };
+
    return (
       <>
          <button
-            className={`flex justify-center items-center gap-3 w-full px-4 py-2 rounded-lg shadow-button whitespace-nowrap ${typeStyles[type]}`}
+            className={`flex justify-center items-center gap-3 w-full ${type ? typeStyles[type] : typeStyles.default} rounded-lg shadow-button whitespace-nowrap`}
             onClick={type !== 'disabled' ? onClick : null}>
-            { item ? <span className='w-[20px]'>{item}</span> : null}
-            <span>{text}</span>
+            {item ? <span className="w-[20px]">{item}</span> : null}
+            <span className={`${size ? sizeStyle[size] : sizeStyle.default}`}>{text}</span>
          </button>
       </>
    );
--- src/shared/ui/types.ts
+++ src/shared/ui/types.ts
@@ -2,7 +2,9 @@ import { ReactNode } from 'react';
 
 // DefaultButton
 type DefaultButtonType = 'default' | 'active' | 'disabled' | 'danger' | 'notSelected';
+type DefaultButtonSize = 'xs' | 'sm' | 'default';
 export type DefaultButtonProps = {
+   size?:DefaultButtonSize;
    type?: DefaultButtonType;
    text: string;
    item?: ReactNode;
