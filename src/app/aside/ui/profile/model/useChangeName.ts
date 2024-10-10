0754d507bf9a1c34d0ba0cb4a2105980f0e2356f
junik <wkdwnsdlr37@gmail.com>
2024-10-10 10:24:14 +0900

    file(aside): useChangeName 삭제

--- src/app/aside/ui/profile/model/useChangeName.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-export const useChangeName = () => {
-   const [isChangeName, setIsChangeName] = useState<boolean>(false);
-   const changeTag = () => setIsChangeName(!isChangeName);
-
-   return { isChangeName, changeTag };
-};
