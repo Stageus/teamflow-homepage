f429ef7acf06cfe396772ee20d7767f40a5a40f5
junik <wkdwnsdlr37@gmail.com>
2024-10-13 14:25:46 +0900

    Design(aside): quick TeamSpaceList 이동

--- src/app/aside/ui/Aside.tsx
+++ src/app/aside/ui/Aside.tsx
@@ -1,21 +1,15 @@
 import { BsArrowDownCircle, BsFillPlusCircleFill } from 'react-icons/bs';
 import { Profile } from './profile';
-import { TeamSpcaeChannel } from "./teamSpaceChannel";
+import { TeamSpcaeChannel } from './teamSpaceChannel';
 
 export const Aside = () => {
    return (
-      <div className="bg-light-bg2 h-screen dark:bg-dark-bg2 flex flex-col gap-2 sticky top-0">
-         <section><Profile/></section>
-         <section className="flex gap-2 h-full p-2">
-            <div className="flex flex-col gap-3">
-               <button className='button-layout button-type-not'>TeamSpaceName</button>
-               <button className='button-layout button-type-not'>TeamSpaceName</button>
-               <button className='button-layout button-type-not'>TeamSpaceName</button>
-               <button className='button-layout button-type-not'>TeamSpaceName</button>
-               <button className='button-layout button-type-not'>TeamSpaceName</button>
-               <button className='text-white bg-gray-600'>테스트</button>
-            </div>
-            <TeamSpcaeChannel/>
+      <div className="sticky top-0 flex h-screen flex-col gap-2 bg-light-bg2 dark:bg-dark-bg2">
+         <section>
+            <Profile />
+         </section>
+         <section className="flex h-full gap-2 p-2">
+            <TeamSpcaeChannel />
          </section>
       </div>
    );
