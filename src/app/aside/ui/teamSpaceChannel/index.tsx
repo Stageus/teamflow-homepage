2659e4ab21c2205048e49aa047577ddc3bd4f20f
junik <wkdwnsdlr37@gmail.com>
2024-10-13 18:56:32 +0900

    Design(aside): teamSpaceChannel 생성

--- src/app/aside/ui/teamSpaceChannel/index.tsx
+++ src/app/aside/ui/teamSpaceChannel/index.tsx
@@ -1,42 +1,145 @@
-import { useState } from 'react';
 import { BsArrowDownCircle, BsFillPlusCircleFill, BsBoxArrowInRight, BsGearFill } from 'react-icons/bs';
 import { useRoute } from '@shared/hooks';
 
 export const TeamSpcaeChannel = () => {
    const { teamspaceRoute, teamspacePublicRoute, teamspacePrivateRoute } = useRoute();
-   const [openChannnelList, setOpenChannelList] = useState(false);
 
    return (
-      <div className="flex grow flex-col gap-3 rounded-lg bg-light-bg3 p-1 dark:bg-dark-bg3">
-         <div
-            className="flex items-center cursor-pointer rounded-lg p-2 h-10 text-primary-gray hover:bg-light-bg3 hover:text-primary-white"
-            onClick={() => teamspaceRoute('teamspaceName')}>
-            <span>공지 채널</span>
+      <>
+         <div className="flex w-fit max-w-40 grow flex-col gap-3">
+            <button className="bg-shade_4 hover:bg-primary sm:text-xs sm:w-10 min-h-10 truncate rounded-full p-2 text-white">
+               TeamSpace1
+            </button>
+            <button className="bg-shade_4 hover:bg-primary sm:text-xs sm:w-10 min-h-10 truncate rounded-full p-2 text-white">
+               TeamSpace2
+            </button>
+            <button className="bg-shade_4 hover:bg-primary sm:text-xs sm:w-10 min-h-10 truncate rounded-full p-2 text-white">
+               TeamSpace3
+            </button>
+            <button className="bg-shade_4 hover:bg-primary sm:text-xs sm:w-10 min-h-10 truncate rounded-full p-2 text-white">
+               TeamSpace4
+            </button>
+            <button className="bg-shade_4 hover:bg-primary sm:text-xs sm:w-10 min-h-10 truncate rounded-full p-2 text-white">
+               TeamSpace5
+            </button>
          </div>
 
-         <div
-            className="flex items-center cursor-pointer rounded-lg p-2 h-10 text-primary-gray hover:bg-light-bg3 hover:text-primary-white"
-            onClick={() => teamspacePublicRoute('teamspaceName')}>
-            <span>공개 채널</span>
-         </div>
+         <div className="bg-shade_2 flex w-full flex-col gap-3 rounded-lg p-1">
+            <div
+               className="h-10 flex-shrink-0 text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center rounded-lg p-2"
+               onClick={() => teamspaceRoute('teamspaceName')}>
+               <span>공지 채널</span>
+            </div>
 
-         <div className="flex justify-between items-center cursor-pointer rounded-lg p-2 h-10 text-primary-gray  hover:text-primary-color">
-            <span className="flex items-center gap-2">
-               비공개 채널 <BsArrowDownCircle />
-            </span>
-            <span className="text-secondary-title text-primary-gray hover:text-primary-color">
-               <BsFillPlusCircleFill />
-            </span>
-         </div>
-         <ul className='px-4'>
-               <li className="flex rounded-lg justify-between items-center cursor-pointer text-primary-gray hover:text-primary-white hover:bg-light-bg3">
-                  <span>비공개 채널이름1</span>
-                  <div className='flex gap-3 text-primary-black dark:text-primary-white'>
-                     <BsBoxArrowInRight size={20}/>
-                     <BsGearFill size={20}/>
+            <div
+               className="h-10 flex-shrink-0 text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center rounded-lg p-2"
+               onClick={() => teamspacePublicRoute('teamspaceName')}>
+               <span>공개 채널</span>
+            </div>
+
+            <div className="h-10 flex-shrink-0 text-shade_5 hover:text-primary flex cursor-pointer items-center justify-between rounded-lg p-2">
+               <span className="flex items-center gap-2">
+                  비공개 채널 <BsArrowDownCircle />
+               </span>
+               <span className="text-shade_5 hover:text-primary h-5 w-5">
+                  <BsFillPlusCircleFill size={'100%'} />
+               </span>
+            </div>
+
+            <ul className="grow px-1 overflow-y-scroll">
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
+                  </div>
+               </li>
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
+                  </div>
+               </li>
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
+                  </div>
+               </li>
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
+                  </div>
+               </li>
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
+                  </div>
+               </li>
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
+                  </div>
+               </li>
+               <li className="text-shade_5 hover:text-primary hover:bg-shade_3 flex cursor-pointer items-center justify-between rounded-lg p-2">
+                  <div>
+                     <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
+                  </div>
+                  <div className="ml-3 flex gap-3 text-black dark:text-white">
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsBoxArrowInRight size={'100%'} />
+                     </div>
+                     <div className="hover:text-primary h-4 w-4">
+                        <BsGearFill size={'100%'} />
+                     </div>
                   </div>
                </li>
             </ul>
-      </div>
+         </div>
+      </>
    );
 };
