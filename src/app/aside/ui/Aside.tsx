d30995e037bb8274ffb56faff1c5d26f3f4294c6
junik <wkdwnsdlr37@gmail.com>
2024-10-14 13:51:20 +0900

    Desing(Aside): layout 구조변경

--- src/app/aside/ui/Aside.tsx
+++ src/app/aside/ui/Aside.tsx
@@ -3,12 +3,12 @@ import { TeamSpcaeChannel } from './teamSpaceChannel';
 
 export const Aside = () => {
    return (
-      <div className="flex flex-col gap-2 bg-shade_1 h-full">
+      <div className="h-full flex flex-col gap-2 bg-shade_1">
          <section>
             <Profile/>
          </section>
-         <section className="grow flex gap-2 p-1 overflow-hidden">
-            <TeamSpcaeChannel/>
+         <section className="grow overflow-hidden">
+               <TeamSpcaeChannel/>
          </section>
       </div>
    );
