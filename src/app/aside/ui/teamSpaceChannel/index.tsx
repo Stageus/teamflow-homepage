042f51d61e41be09411ca925bdc4b4ae4389050b
junik <wkdwnsdlr37@gmail.com>
2024-10-16 12:09:32 +0900

    Design(aside): 생성 및 나가기 모달추가

--- src/app/aside/ui/teamSpaceChannel/index.tsx
+++ src/app/aside/ui/teamSpaceChannel/index.tsx
@@ -20,10 +20,13 @@ export const TeamSpcaeChannel = () => {
    const onClickIsModal = () => setCreateChannelModal(!createChannelModal);
    const inputRef = useRef(null);
 
+   const [outSideChannelModal, setOutSideChannelModal] = useState(false);
+   const onClickIsOutside = () => setOutSideChannelModal(!outSideChannelModal);
+
    return (
       <div className="flex h-full gap-3 p-2">
-         <div className="flex items-center w-full max-w-[100px] shrink grow flex-col gap-3 overflow-y-scroll">
-            <button className="bg-shade_4 hover:bg-primary min-h-10 w-full sm:w-10 truncate rounded-full p-2 text-sm text-white">
+         <div className="flex w-full max-w-[100px] shrink grow flex-col items-center gap-3 overflow-y-scroll">
+            <button className="bg-shade_4 hover:bg-primary sm:w-10 min-h-10 w-full truncate rounded-full p-2 text-sm text-white">
                TeamSpace1
             </button>
          </div>
@@ -53,19 +56,19 @@ export const TeamSpcaeChannel = () => {
                   <BsFillPlusCircleFill size={'100%'} />
                </span>
                {/* create channel modal */}
-               <FullScreenModal title="비공개 채널생성dddddddfasdfsdfdsf" isModal={createChannelModal} closeModal={onClickIsModal}>
-                  <div className='w-[300px] flex flex-col gap-6 items-center'>
-                  <h2 className="text-shade_5">비공개 채널을 생성합니다.</h2>
-                  <TextInput
-                     inputRef={inputRef}
-                     placeholder='사용할 비공개 채널이름을 입력해주세요'
-                     nextName='생성'
-                     nextCallback={()=>console.log("생성동작")}
-                     cancellName='취소'
-                     cancellCallback={onClickIsModal}
-                     regex={/^[가-힣a-zA-Z0-9]{5,20}$/}
-                     regexText="5글자 이상 ~ 20글자 이하 (한글/영어/숫자)만 가능합니다"
-                  />
+               <FullScreenModal title="비공개 채널생성" isModal={createChannelModal} closeModal={onClickIsModal}>
+                  <div className="flex w-[300px] flex-col items-center gap-6">
+                     <h2 className="text-shade_5">비공개 채널을 생성합니다.</h2>
+                     <TextInput
+                        inputRef={inputRef}
+                        placeholder="사용할 비공개 채널이름을 입력해주세요"
+                        nextName="생성"
+                        nextCallback={() => console.log('생성동작')}
+                        cancellName="취소"
+                        cancellCallback={onClickIsModal}
+                        regex={/^[가-힣a-zA-Z0-9]{5,20}$/}
+                        regexText="5글자 이상 ~ 20글자 이하 (한글/영어/숫자)만 가능합니다"
+                     />
                   </div>
                </FullScreenModal>
             </div>
@@ -77,7 +80,7 @@ export const TeamSpcaeChannel = () => {
                         <span className="line-clamp-1 text-sm">비공개 채널이름1</span>
                      </div>
                      <div className="ml-3 flex gap-3 text-black dark:text-white">
-                        <div className="hover:text-primary h-4 w-4" onClick={() => console.log('채널 나가기')}>
+                        <div className="hover:text-primary h-4 w-4" onClick={onClickIsOutside}>
                            <BsBoxArrowInRight size={'100%'} />
                         </div>
                         <div className="hover:text-primary h-4 w-4" onClick={() => console.log('채널관리 모달')}>
@@ -87,6 +90,15 @@ export const TeamSpcaeChannel = () => {
                   </li>
                ) : null}
             </ul>
+            <FullScreenModal title="채널나가기" isModal={outSideChannelModal} closeModal={onClickIsOutside}>
+               <div className="flex w-[300px] flex-col items-center gap-6">
+                  <h2 className="text-shade_5">{'정말 (채널이름) 을 나가겠습니까?'}</h2>
+                  <div className='flex gap-3'>
+                  <button className="button-layout _danger" onClick={()=>console.log("채널나가기 동작")}>나가기</button>
+                  <button className="button-layout _default" onClick={onClickIsOutside}>취소</button>
+                  </div>
+               </div>
+            </FullScreenModal>
          </div>
       </div>
    );
