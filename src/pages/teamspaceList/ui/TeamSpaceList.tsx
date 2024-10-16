ca15b7eb4d8ae0d945110c1486b9ddb014e46d94
junik <wkdwnsdlr37@gmail.com>
2024-10-16 12:39:08 +0900

    Refactor(teamSpaceList): TeamSpace 생성모달창 로직분리

--- src/pages/teamspaceList/ui/TeamSpaceList.tsx
+++ src/pages/teamspaceList/ui/TeamSpaceList.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
-import { BsFillPlusCircleFill, BsGearFill, BsXLg } from 'react-icons/bs';
+import { BsFillPlusCircleFill, BsGearFill } from 'react-icons/bs';
+import { useCreateTeamSpace } from "../model/useCreateTeamSpace";
+import { TextInput } from '@features/textInput';
+import { FullScreenModal } from '@shared/ui';
 
 export const TeamSpaceList = () => {
-   const [isTeamSpaceModal, setIsTeamSpaceModal] = useState(false);
-   const onClickOpenModal = () => setIsTeamSpaceModal(!isTeamSpaceModal);
+   const { createTeamSpaceModal, onClickIsModal, inputRef } = useCreateTeamSpace();
 
    return (
       <div className="h-full p-5">
          <h1 className="py-2">나의 TeamSpcae</h1>
          {/* TeamSpace create button */}
-         <div className="card-gradient-light dark:card-gradient-dark text-primary group h-32 cursor-pointer rounded-lg p-4 hover:text-white" onClick={onClickOpenModal}>
+         <div
+            className="card-gradient-light dark:card-gradient-dark text-primary group h-32 cursor-pointer rounded-lg p-4 hover:text-white"
+            onClick={onClickIsModal}>
             <h2 className="italic">TeamSpace 생성</h2>
             <span className="p-1 text-xs">지금바로 나만의 팀을 만들어 보세요</span>
             <div className="flex items-center justify-end gap-3">
@@ -18,14 +21,21 @@ export const TeamSpaceList = () => {
             </div>
          </div>
          {/* TeamSpace create modal */}
-         <div className={`full-screen-modal ${isTeamSpaceModal ? '_show' : '_hidden'}`}>
-            <h1 className="title">TeamSpace 생성</h1>
-            {/* <div
-               onClick={onClickOpenModal}
-               className="hover:text-secondary absolute right-5 top-5 flex h-[25px] w-[25px] cursor-pointer items-center justify-end text-white">
-               <BsXLg size={'100%'} />
-            </div> */}
-         </div>
+         <FullScreenModal title="TeamSpace 생성" isModal={createTeamSpaceModal} closeModal={onClickIsModal}>
+            <div className="flex w-[300px] flex-col items-center gap-6">
+               <h2 className="text-white">TeamSpace를 생성합니다.</h2>
+               <TextInput
+                  inputRef={inputRef}
+                  placeholder="사용할 TeamSpace 채널이름을 입력해주세요"
+                  nextName="생성"
+                  nextCallback={() => console.log('생성동작')}
+                  cancellName="취소"
+                  cancellCallback={onClickIsModal}
+                  regex={/^[가-힣a-zA-Z0-9]{5,20}$/}
+                  regexText="5글자 이상 ~ 20글자 이하 (한글/영어/숫자)만 가능합니다"
+               />
+            </div>
+         </FullScreenModal>
 
          <ul className="flex max-w-3xl flex-col gap-3 py-2">
             <li className="flex gap-2">
