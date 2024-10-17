754b05898bf4e8e0a2f7fc91e965c64280f1f7a9
junik <wkdwnsdlr37@gmail.com>
2024-10-17 13:39:58 +0900

    Desing(thread): 생성

--- src/widgets/thread/ui/Thread.tsx
+++ src/widgets/thread/ui/Thread.tsx
@@ -7,52 +7,85 @@ export const Thread = () => {
    const onClickMoreText = () => setTextLine(!textLine);
    return (
       <>
-         <div className="bg-shade_4 flex flex-col gap-2 rounded-lg p-2">
-            <div className="flex items-center justify-between">
-               <div className="flex items-center gap-2">
-                  <div className="h-12 w-12 rounded-full">
-                     <img className="h-full w-full rounded-full" src={testImg} />
+         <div className="bg-shade_2 flex w-full gap-4 rounded-lg px-5 py-3 shadow-md">
+            {/* entities area */}
+            <div className="flex grow flex-col gap-3">
+               {/* 작성자이미지, 작성자이름, 작성날짜 */}
+               <div className="flex items-center gap-3">
+                  <div className="h-9 w-9">
+                     <img className="service-img" src={testImg} />
                   </div>
-                  <div className="text-primary">
-                     <span>작성자이름</span>
+                  <div>
+                     <span className="text-gray">작성자이름</span>
                   </div>
-                  <div className="text-shade_5">
-                     <span>2024-01-01</span>
+                  <div>
+                     <span className="text-shade_5 text-xs">2024-01-01</span>
                   </div>
                </div>
-               <div className="flex gap-2">
-                  <span className="hover:text-secondary h-5 w-5 cursor-pointer text-black dark:text-white">
+               {/* thred 내용 */}
+               <p
+                  className={`dark:text-gray text-justify text-sm leading-6 text-black ${textLine ? 'line-clamp-none max-h-full' : 'line-clamp-1 max-h-6'} transition-all duration-150`}>
+                  모든 사용자는 다양한 정보와 데이터를 바탕으로 의사결정을 내리게 됩니다. 따라서 효율적인 정보 관리와
+                  전달은 현대 사회에서 필수적인 요소로 자리 잡고 있습니다. 특히 디지털 환경이 발전함에 따라 신속한
+                  데이터 접근과 정밀한 분석이 가능해졌으며, 이를 통해 개인과 기업은 더 나은 성과를 도출할 수 있습니다.
+                  정보의 홍수 속에서 중요한 것은 올바른 데이터를 선별하고 필요한 순간에 적절히 활용하는 것입니다. 또한
+                  협업 도구와 클라우드 서비스의 발전으로 원격 근무 환경에서도 다양한 정보 자원을 손쉽게 관리할 수 있게
+                  되었습니다. 그러나 이러한 편리함에는 보안과 프라이버시 보호에 대한 문제도 함께 따릅니다. 안전한 정보
+                  관리는 단순히 기술적 접근만으로 해결되지 않으며, 정책과 윤리적인 관점에서도 고려가 필요합니다. 앞으로
+                  정보 활용이 더욱 중요해질수록 우리는 데이터의 신뢰성과 보안을 지속적으로 강화해야 합니다. 이는 기술의
+                  발전과 함께 규제와 법률이 조화를 이루어야 함을 의미하며, 궁극적으로는 정보의 투명성과 사용자의 권리를
+                  보장하는 방향으로 나아가야 합니다. 기업과 개인은 정보 보호를 최우선 과제로 삼아야 하며, 신뢰할 수 있는
+                  정보 환경을 구축하기 위해 모두가 노력해야 합니다.모든 사용자는 다양한 정보와 데이터를 바탕으로
+                  의사결정을 내리게 됩니다. 따라서 효율적인 정보 관리와 전달은 현대 사회에서 필수적인 요소로 자리 잡고
+                  있습니다. 특히 디지털 환경이 발전함에 따라 신속한 데이터 접근과 정밀한 분석이 가능해졌으며, 이를 통해
+                  개인과 기업은 더 나은 성과를 도출할 수 있습니다. 정보의 홍수 속에서 중요한 것은 올바른 데이터를
+                  선별하고 필요한 순간에 적절히 활용하는 것입니다. 또한 협업 도구와 클라우드 서비스의 발전으로 원격 근무
+                  환경에서도 다양한 정보 자원을 손쉽게 관리할 수 있게 되었습니다. 그러나 이러한 편리함에는 보안과
+                  프라이버시 보호에 대한 문제도 함께 따릅니다. 안전한 정보 관리는 단순히 기술적 접근만으로 해결되지
+                  않으며, 정책과 윤리적인 관점에서도 고려가 필요합니다. 앞으로 정보 활용이 더욱 중요해질수록 우리는
+                  데이터의 신뢰성과 보안을 지속적으로 강화해야 합니다. 이는 기술의 발전과 함께 규제와 법률이 조화를
+                  이루어야 함을 의미하며, 궁극적으로는 정보의 투명성과 사용자의 권리를 보장하는 방향으로 나아가야
+                  합니다. 기업과 개인은 정보 보호를 최우선 과제로 삼아야 하며, 신뢰할 수 있는 정보 환경을 구축하기 위해
+                  모두가 노력해야 합니다.
+               </p>
+               {/* 파일목록 */}
+               <div className="flex gap-3">
+                  <span className="bg-shade_3 theme-text flex items-center justify-start rounded-lg p-2 text-xs">
+                     파일이름1
+                  </span>
+                  <span className="bg-shade_3 theme-text flex items-center justify-start rounded-lg p-2 text-xs">
+                     파일이름2
+                  </span>
+                  <span className="bg-shade_3 theme-text flex items-center justify-start rounded-lg p-2 text-xs">
+                     파일이름3
+                  </span>
+               </div>
+            </div>
+
+            {/* features area */}
+            {/* 파일다운로드 버튼, 수정버튼, 삭제버튼 */}
+            <div className="flex flex-col gap-3">
+               <div className='basis-9 flex items-center gap-4'>
+                  <span className="theme-text theme-hover-text h-4 w-4">
                      <BsFileEarmarkDiff size={'100%'} />
                   </span>
-                  <span className="hover:text-secondary h-5 w-5 cursor-pointer text-black dark:text-white">
+                  <span className="theme-text theme-hover-text h-4 w-4">
                      <BsFillPencilFill size={'100%'} />
                   </span>
-                  <span className="hover:text-secondary h-5 w-5 cursor-pointer text-black dark:text-white">
+                  <span className="theme-text theme-hover-text h-4 w-4">
                      <BsTrash3 size={'100%'} />
                   </span>
                </div>
-            </div>
-            <div>
-               <p
-                  className={`text-black dark:text-white ${textLine ? 'line-clamp-none max-h-40 overflow-visible' : 'line-clamp-2 max-h-9 overflow-hidden'} transition-all duration-200`}>
-                  사용자 친화적 인터페이스로 직관적이고 깔금한 Ui를 통해 사용자들이 기능을 쉽게 이해하고 활용할수 있도록
-                  설계되었습니다. 실시간 기능 중심이며, 실시간 채팅 알림등 즉각적인 피드백이 필요한 기능들을 사용할수가
-                  있습니다 이로인해 팀원 간의 신속한 소통을 지원합니다.사용자 친화적 인터페이스로 직관적이고 깔금한 Ui를
-                  통해 사용자들이 기능을 쉽게 이해하고 활용할수 있도록 설계되었습니다. 실시간 기능 중심이며, 실시간 채팅
-                  알림등 즉각적인 피드백이 필요한 기능들을 사용할수가 있습니다 이로인해 팀원 간의 신속한 소통을
-                  지원합니다. 끝!
-               </p>
-               <span className="text-secondary cursor-pointer" onClick={onClickMoreText}>
-                  {textLine ? '접기' : '더보기'}
-               </span>
-            </div>
-            <div className="max-w-screen-md flex gap-2 text-black dark:text-white">
-               <div className="bg-shade_5 grow rounded-lg p-2 text-white">파일이름</div>
-               <div className="bg-shade_5 grow rounded-lg p-2 text-white">파일이름</div>
-               <div className="bg-shade_5 grow rounded-lg p-2 text-white">파일이름</div>
+               <div className="whitespace-nowrap leading-6">
+                     <span
+                        className={`theme-hover-text text-gray self-end font-bold ${textLine ? 'text-primary' : ''}`}
+                        onClick={onClickMoreText}>
+                        {textLine ? '접기' : '더보기'}
+                     </span>
+                  </div>
             </div>
          </div>
-         <div className='flex flex-col gap-2 border-primary border-4 rounded-lg p-3 h-72'>
+         {/* <div className='flex flex-col gap-2 border-primary border-4 rounded-lg p-3 h-72'>
             <div className='flex items-center gap-2'><span className='text-xs text-shade_5'>모두에게 멘션</span><input type='checkbox'/></div>
             <textarea className='grow w-full text-left bg-transparent outline-none text-black dark:text-white border-b-2 border-shade_5 resize-none' placeholder='최대 1000자까지 입력 가능합니다'/>
             <div className='flex items-center gap-2'>
@@ -61,7 +94,7 @@ export const Thread = () => {
                      <BsFileEarmarkDiff size={'100%'} />
                 </span>
             </div>
-         </div>
+         </div> */}
       </>
    );
 };
