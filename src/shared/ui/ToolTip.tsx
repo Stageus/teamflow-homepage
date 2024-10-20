fd02ae442e217968a5f4fe18f511bdba44dbc877
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-10-20 18:33:25 +0900

    Tooltip 생성 (#44)
    * Docs(package): react-tooltip 설치
    
    * Docs(README): link 추가
    
    * Design(global.css): tooltip class 추가:
    
    * Desing(TooTip): 생성

---
+++ src/shared/ui/ToolTip.tsx
@@ -0,0 +1,17 @@
+import { useId } from 'react';
+import { Tooltip } from 'react-tooltip';
+import {TooTipProps} from "./types";
+
+export const ToolTip = (props : TooTipProps) => {
+   const { toolTipContent, children, place} = props;
+   const uniqueId = useId();
+
+   return (
+      <>
+        <div data-tooltip-id={uniqueId} className='tooltip'>
+            {children}
+            <Tooltip id={uniqueId} content={toolTipContent} place={place ?? "top"} className='item'/>
+        </div>
+      </>
+   );
+};
