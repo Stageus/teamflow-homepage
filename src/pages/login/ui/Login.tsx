6bc4b4d4c1d6e6e8a5b16d82b09162a1e67c98b1
junik <wkdwnsdlr37@gmail.com>
2024-10-20 19:31:21 +0900

    None

--- src/pages/login/ui/Login.tsx
+++ src/pages/login/ui/Login.tsx
@@ -1,21 +1,21 @@
-import googleLogo from "../assets/google.svg";
-import serviceLogo from "@shared/assets/logo.svg";
-import { useRoute } from "@shared/hooks";
+import googleLogo from '../assets/google.svg';
+import serviceLogo from '@shared/assets/logo.svg';
+import { useRoute } from '@shared/hooks';
 
 export const Login = () => {
    const { signupRoute } = useRoute();
 
    return (
-      <div className="h-screen flex justify-center items-center gradient-light dark:gradient-dark">
-         <div className="flex flex-col justify-center items-center gap-5 px-10 py-5 bg-transparent rounded-lg shadow-xl min-w-[300px] min-h-[300px]">
-            <div className="w-20 h-20">
-               <img src={serviceLogo}/>
+      <div className="gradient-light dark:gradient-dark flex h-screen items-center justify-center">
+         <div className="flex min-h-[300px] min-w-[300px] flex-col items-center justify-center gap-5 rounded-lg bg-transparent px-10 py-5 shadow-xl">
+            <div className="h-20 w-20">
+               <img src={serviceLogo} />
             </div>
             <h1 className="italic">TeamFlow</h1>
             <h2 className="text-shade_5">TeamFlow에 오신것을 환영합니다!</h2>
-            <button className='button-layout _active' onClick={signupRoute}>
-               <div className="w-6 h-6">
-                  <img src={googleLogo}/>
+            <button className="button-layout _active" onClick={signupRoute}>
+               <div className="h-6 w-6">
+                  <img src={googleLogo} />
                </div>
                <span>Google 로그인</span>
             </button>
