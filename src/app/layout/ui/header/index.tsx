41e3f907749f972baad0d93218290d9561de10ad
junik <wkdwnsdlr37@gmail.com>
2024-10-20 22:05:51 +0900

    Refactor(header): index.tsx cookie 적용

--- src/app/layout/ui/header/index.tsx
+++ src/app/layout/ui/header/index.tsx
@@ -1,11 +1,12 @@
 import { AlarmModal } from './ui/alarmModal';
-import { useChangeTheme } from "./model/useChangeTheme";
+import { useChangeTheme } from './model/useChangeTheme';
 import { useLogout } from './model/useLogout';
 import { useUrlTracking } from '@shared/store/urlTracking';
 import { PATHS } from '@shared/consts/paths';
-import { useModalEventDetect } from '@shared/hooks/useModalEventDetect';
+import { useModalEventDetect } from './model/useModalEventDetect';
 import { useRoute } from '@shared/hooks/useRoute';
 import { FullScreenModal } from '@shared/ui/FullScreenModal';
+import { useCookie } from '@shared/hooks/useCookie';
 
 export const Header = () => {
    const { loginRoute, teamspaceListRoute } = useRoute();
@@ -13,6 +14,7 @@ export const Header = () => {
    const { modalRef, isModalDetect, onClickOpenModal } = useModalEventDetect();
    const { isLogout, onClickOpenLogoutModal } = useLogout();
    const { theme, onClickchangeTheme } = useChangeTheme();
+   const { cookies, removeCookieName } = useCookie('token');
 
    return (
       <div className="flex justify-start gap-3 whitespace-nowrap px-4 py-2">
@@ -37,29 +39,38 @@ export const Header = () => {
                {theme ? '라이트모드' : '다크모드'}
             </button>
          </div>
+
          <div>
-            <button className="button-layout _not-select sm:px-2 sm:py-2 sm:text-xs" onClick={loginRoute}>
-               로그인
-            </button>
+            {cookies.token ? (
+               <button
+                  className="button-layout _not-select sm:px-2 sm:py-2 sm:text-xs"
+                  onClick={onClickOpenLogoutModal}>
+                  로그아웃
+               </button>
+            ) : (
+               <button className="button-layout _not-select sm:px-2 sm:py-2 sm:text-xs" onClick={loginRoute}>
+                  로그인
+               </button>
+            )}
          </div>
-         <div>
-            <button className="button-layout _not-select sm:px-2 sm:py-2 sm:text-xs" onClick={onClickOpenLogoutModal}>
-               로그아웃
-            </button>
-            <FullScreenModal title="Logout" isModal={isLogout} closeModal={onClickOpenLogoutModal}>
-               <div className='flex flex-col gap-8'>
-                  <h2 className="text-white">로그아웃 하시겠습니까?</h2>
-                  <div className="flex gap-5">
-                     <button className="button-layout _danger" onClick={loginRoute}>
-                        로그아웃
-                     </button>
-                     <button className="button-layout _default" onClick={onClickOpenLogoutModal}>
-                        되돌아가기
-                     </button>
-                  </div>
+         <FullScreenModal title="Logout" isModal={isLogout} closeModal={onClickOpenLogoutModal}>
+            <div className="flex flex-col gap-8">
+               <h2 className="text-white">로그아웃 하시겠습니까?</h2>
+               <div className="flex gap-5">
+                  <button
+                     className="button-layout _danger"
+                     onClick={() => {
+                        removeCookieName('token');
+                        loginRoute();
+                     }}>
+                     로그아웃
+                  </button>
+                  <button className="button-layout _default" onClick={onClickOpenLogoutModal}>
+                     되돌아가기
+                  </button>
                </div>
-            </FullScreenModal>
-         </div>
+            </div>
+         </FullScreenModal>
       </div>
    );
 };
