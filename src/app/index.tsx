b8e2f2410671ae1cec0f0ff93af8fe1ddb8c9d34
junik <wkdwnsdlr37@gmail.com>
2024-10-20 21:45:12 +0900

    Refactor(app): themeProvider 제거

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import './global.css';
 import { Router } from "./Router";
-import { DarkThemeProvider } from "@shared/store/darkTheme";
 
 function App() {
   return (
     <>
-    {/* <DarkThemeProvider> */}
       <RouterProvider router={Router}/>
-    {/* </DarkThemeProvider> */}
     </>
   )
 }
