7f21de5f417c3101d20af24b6604439168cfdccd
junik <wkdwnsdlr37@gmail.com>
2024-11-06 20:55:27 +0900

    Refactor(app): userInfo entities 적용

--- src/app/layout/ui/aside/ui/userInfo/index.tsx
+++ src/app/layout/ui/aside/ui/userInfo/index.tsx
@@ -1,26 +1,24 @@
 import { useState, useRef } from 'react';
 import { BsGearFill, BsXLg } from 'react-icons/bs';
 import { TextInput } from '@features/textInput';
+import { useGetUsers } from '@entities/useGetUsers';
+import { usePutUsersProfileImg } from '@entities/usePutUsersProfileImg';
 import { RoundProfile } from '@shared/ui/RoundProfile';
+
 export const UserInfo = () => {
+   
    const [isNameInput, setIsNameInput] = useState<boolean>(false);
    const toggleNameInput = () => setIsNameInput(!isNameInput);
-
    const [isMoreInfo, setIsMoreInfo] = useState<boolean>(false);
    const toggleMoreInfo = () => {
       setIsMoreInfo(!isMoreInfo);
       setIsNameInput(false);
    };
 
-   const inputFileRef = useRef(null);
+   const { profileInfo } = useGetUsers();
+   const { profileImg, selectFile, cancelFile, putUsersProfileImg } = usePutUsersProfileImg();   
+   
    const inputRef = useRef(null);
-   const _dataEntry = {
-      imgUrl: null,
-      userEmail: 'userEmail@gmail.com',
-      userNickName: '테스트유저 닉네임',
-      myTeamSpace: 250,
-      guestTeamSpace: 240,
-   };
 
    return (
       <>
@@ -29,11 +27,12 @@ export const UserInfo = () => {
             <div className="flex gap-2">
                {/* 유저 profileImg*/}
                <div className="w-12 h-12">
-                  <RoundProfile imgUrl={_dataEntry.imgUrl} />
+                  {/* 유저 이미지가 있을경우, 유저가 새로운 이미지를 선택했을경우, */}
+                  <RoundProfile imgUrl={profileImg.preview ? profileImg.preview : profileInfo.profileImage} />
                </div>
                <div className="flex flex-col justify-between gap-2 grow">
                   {/* 유저 email */}
-                  <span className="text-shade_5">{_dataEntry.userEmail}</span>{' '}
+                  <span className="text-shade_5">{profileInfo.email}</span>{' '}
                   <div className={`${isNameInput && isMoreInfo ? 'h-24' : 'h-4'} duration-200`}>
                      {isNameInput && isMoreInfo ? (
                         // 이름 변경 input
@@ -51,25 +50,42 @@ export const UserInfo = () => {
                         // 유저 NickName
                         <span
                            className={`${isNameInput && isMoreInfo ? 'invisible max-h-0 opacity-0 duration-75' : 'visible max-h-20 py-2 opacity-100 duration-200'} text-primary transition-all`}>
-                           {_dataEntry.userNickName}
+                           {profileInfo.nickname}
                         </span>
                      )}
                   </div>
                   <div
                      className={`${isMoreInfo && !isNameInput ? 'visible max-h-20 py-2 opacity-100 duration-200' : 'invisible max-h-0 opacity-0 duration-75'} flex gap-2 transition-all`}>
-                     {/* 이미지 변경 버튼 */}
-                     <div>
-                        <label htmlFor="file" className="px-2 py-1 text-xs button-layout _active">
-                           이미지 변경
-                        </label>
-                        <input id="file" type="file" ref={inputFileRef} className="hidden" />
-                     </div>
-                     {/* 이름 변경 버튼 */}
-                     <div>
-                        <button className="px-2 py-1 text-xs button-layout _active" onClick={toggleNameInput}>
-                           닉네임 변경
-                        </button>
-                     </div>
+                     { profileImg.fileObj && profileImg.preview ? (
+                        <>
+                           <div>
+                              <button className="p-2 text-xs button-layout _active" onClick={putUsersProfileImg}>
+                                 확인
+                              </button>
+                           </div>
+                           <div>
+                              <button className="p-2 text-xs button-layout _default" onClick={cancelFile}>
+                                 취소
+                              </button>
+                           </div>
+                        </>
+                     ) : (
+                        <>
+                           {/* 이미지 변경 버튼 */}
+                           <div>
+                              <label htmlFor="user_img" className="px-2 py-1 text-xs button-layout _active">
+                                 이미지 변경
+                              </label>
+                              <input id="user_img" type="file" className="hidden" onChange={selectFile} />
+                           </div>
+                           {/* 이름 변경 버튼 */}
+                           <div>
+                              <button className="px-2 py-1 text-xs button-layout _active" onClick={toggleNameInput}>
+                                 닉네임 변경
+                              </button>
+                           </div>
+                        </>
+                     )}
                   </div>
                </div>
                {/* 유저정보 더보기 버튼*/}
@@ -79,17 +95,19 @@ export const UserInfo = () => {
                   {isMoreInfo ? <BsXLg size={'100%'} /> : <BsGearFill size={'100%'} />}
                </div>
             </div>
+            {/* 유저이미 유효성검사 텍스트 */}
+            {profileImg.errorType && <p className="text-xs text-error">{profileImg.errorType}</p>}
 
             {/* 유저 TeamSpace정보 영역*/}
             <div
                className={`${isMoreInfo ? 'visible max-h-20 py-2 opacity-100 duration-200' : 'invisible max-h-0 opacity-0 duration-75'} flex gap-3 transition-all`}>
                <div className="flex flex-col gap-1 p-2 rounded-lg grow bg-shade_2">
                   <span className="text-sm text-primary">나의 TeamSpace</span>
-                  <span className="text-black dark:text-white">{_dataEntry.myTeamSpace}</span>
+                  <span className="text-black dark:text-white">{profileInfo.teamSpaceOwnCount}</span>
                </div>
                <div className="flex flex-col gap-1 p-2 rounded-lg grow bg-shade_2">
                   <span className="text-sm text-primary">참여중인 TeamSpace</span>
-                  <span className="text-black dark:text-white">{_dataEntry.guestTeamSpace}</span>
+                  <span className="text-black dark:text-white">{profileInfo.teamSpaceCount}</span>
                </div>
             </div>
          </div>
