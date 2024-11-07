b2d044457851830a457ae1c619d855a05bb2968d
junik <wkdwnsdlr37@gmail.com>
2024-11-08 05:25:40 +0900

    Refactor(add): useToogle 로직 분리

--- src/app/layout/ui/aside/ui/userInfo/index.tsx
+++ src/app/layout/ui/aside/ui/userInfo/index.tsx
@@ -1,24 +1,23 @@
-import { useState, useRef } from 'react';
+// npm
 import { BsGearFill, BsXLg } from 'react-icons/bs';
-import { TextInput } from '@features/textInput';
+// slice
+import { useToogle } from './model/useToogle';
+import { useProfileImg } from './model/useProfileImg';
+// layer
 import { useGetUsers } from '@entities/useGetUsers';
 import { usePutUsersProfileImg } from '@entities/usePutUsersProfileImg';
 import { RoundProfile } from '@shared/ui/RoundProfile';
+import { useValidation } from '@shared/hooks/useValidation';
+import { nickNameRegExp } from '@shared/consts/regExp';
 
 export const UserInfo = () => {
-   
-   const [isNameInput, setIsNameInput] = useState<boolean>(false);
-   const toggleNameInput = () => setIsNameInput(!isNameInput);
-   const [isMoreInfo, setIsMoreInfo] = useState<boolean>(false);
-   const toggleMoreInfo = () => {
-      setIsMoreInfo(!isMoreInfo);
-      setIsNameInput(false);
-   };
-
+   // ui
+   const { inputRef, isValid, isCheckValidation } = useValidation(nickNameRegExp);
+   const { toggleState, changeToggleState } = useToogle(inputRef);
+   const { profileImg, selectFile, cancelFile } = useProfileImg();
+   // api
    const { profileInfo } = useGetUsers();
-   const { profileImg, selectFile, cancelFile, putUsersProfileImg } = usePutUsersProfileImg();   
-   
-   const inputRef = useRef(null);
+   const { putUsersProfileImg } = usePutUsersProfileImg();
 
    return (
       <>
@@ -27,36 +26,56 @@ export const UserInfo = () => {
             <div className="flex gap-2">
                {/* 유저 profileImg*/}
                <div className="w-12 h-12">
-                  {/* 유저 이미지가 있을경우, 유저가 새로운 이미지를 선택했을경우, */}
-                  <RoundProfile imgUrl={profileImg.preview ? profileImg.preview : profileInfo.profileImage} />
+                  <RoundProfile imgUrl={profileImg.preview ?? profileInfo?.profileImage} />
                </div>
                <div className="flex flex-col justify-between gap-2 grow">
                   {/* 유저 email */}
-                  <span className="text-shade_5">{profileInfo.email}</span>{' '}
-                  <div className={`${isNameInput && isMoreInfo ? 'h-24' : 'h-4'} duration-200`}>
-                     {isNameInput && isMoreInfo ? (
-                        // 이름 변경 input
-                        <TextInput
-                           inputRef={inputRef}
-                           nextName="저장"
-                           nextCallback={() => console.log('저장시 동작')}
-                           cancellName="취소"
-                           cancellCallback={toggleNameInput}
-                           placeholder="이전 닉네임"
-                           regex={/^[가-힣a-zA-Z0-9]{3,10}$/}
-                           regexText="3글자 이상 ~ 10글자 이하"
-                        />
+                  <span className="text-shade_5">{profileInfo?.email}</span>
+                  <div
+                     className={`${toggleState.isNickNameInput && toggleState.isMoreInfo ? 'h-24' : 'h-4'} duration-200`}>
+                     {toggleState.isNickNameInput && toggleState.isMoreInfo ? (
+                        <div className='flex flex-col gap-1'>
+                           <input
+                              type="text"
+                              ref={inputRef}
+                              onChange={isCheckValidation}
+                              defaultValue={profileInfo?.nickname}
+                              placeholder="3글자 이상 ~ 10글자 이하"
+                              className={`input-layout ${inputRef.current?.value && (isValid ? 'border-success' : 'border-error')}`}
+                           />
+                           {inputRef.current?.value && (
+                              <p className={`text-xs ${isValid ? 'text-success' : 'text-error'}`}>
+                                 {isValid ? '사용가능합니다' : '3글자 이상 ~ 10글자 이하'}
+                              </p>
+                           )}
+                           <div className="flex justify-start gap-3 py-2">
+                              <div>
+                                 <button
+                                    className={`button-layout ${isValid ? '_active' : '_disabled'}`}
+                                    onClick={() => ''}>
+                                    확인
+                                 </button>
+                              </div>
+                              <div>
+                                 <button
+                                    className="button-layout _default"
+                                    onClick={() => changeToggleState('isNickNameInput')}>
+                                    취소
+                                 </button>
+                              </div>
+                           </div>
+                        </div>
                      ) : (
                         // 유저 NickName
                         <span
-                           className={`${isNameInput && isMoreInfo ? 'invisible max-h-0 opacity-0 duration-75' : 'visible max-h-20 py-2 opacity-100 duration-200'} text-primary transition-all`}>
-                           {profileInfo.nickname}
+                           className={`${toggleState.isNickNameInput && toggleState.isMoreInfo ? 'invisible max-h-0 opacity-0 duration-75' : 'visible max-h-20 py-2 opacity-100 duration-200'} text-primary transition-all`}>
+                           {profileInfo?.nickname}
                         </span>
                      )}
                   </div>
                   <div
-                     className={`${isMoreInfo && !isNameInput ? 'visible max-h-20 py-2 opacity-100 duration-200' : 'invisible max-h-0 opacity-0 duration-75'} flex gap-2 transition-all`}>
-                     { profileImg.fileObj && profileImg.preview ? (
+                     className={`${toggleState.isMoreInfo && !toggleState.isNickNameInput ? 'visible max-h-20 py-2 opacity-100 duration-200' : 'invisible max-h-0 opacity-0 duration-75'} flex gap-2 transition-all`}>
+                     {profileImg.fileObj && profileImg.preview ? (
                         <>
                            <div>
                               <button className="p-2 text-xs button-layout _active" onClick={putUsersProfileImg}>
@@ -80,7 +99,9 @@ export const UserInfo = () => {
                            </div>
                            {/* 이름 변경 버튼 */}
                            <div>
-                              <button className="px-2 py-1 text-xs button-layout _active" onClick={toggleNameInput}>
+                              <button
+                                 className="px-2 py-1 text-xs button-layout _active"
+                                 onClick={() => changeToggleState('isNickNameInput')}>
                                  닉네임 변경
                               </button>
                            </div>
@@ -90,9 +111,9 @@ export const UserInfo = () => {
                </div>
                {/* 유저정보 더보기 버튼*/}
                <div
-                  onClick={toggleMoreInfo}
+                  onClick={() => changeToggleState('isMoreInfo')}
                   className="flex items-center justify-end w-5 h-5 text-black cursor-pointer dark:text-white">
-                  {isMoreInfo ? <BsXLg size={'100%'} /> : <BsGearFill size={'100%'} />}
+                  {toggleState.isMoreInfo ? <BsXLg size={'100%'} /> : <BsGearFill size={'100%'} />}
                </div>
             </div>
             {/* 유저이미 유효성검사 텍스트 */}
@@ -100,14 +121,14 @@ export const UserInfo = () => {
 
             {/* 유저 TeamSpace정보 영역*/}
             <div
-               className={`${isMoreInfo ? 'visible max-h-20 py-2 opacity-100 duration-200' : 'invisible max-h-0 opacity-0 duration-75'} flex gap-3 transition-all`}>
+               className={`${toggleState.isMoreInfo ? 'visible max-h-20 py-2 opacity-100 duration-200' : 'invisible max-h-0 opacity-0 duration-75'} flex gap-3 transition-all`}>
                <div className="flex flex-col gap-1 p-2 rounded-lg grow bg-shade_2">
                   <span className="text-sm text-primary">나의 TeamSpace</span>
-                  <span className="text-black dark:text-white">{profileInfo.teamSpaceOwnCount}</span>
+                  <span className="text-black dark:text-white">{profileInfo?.teamSpaceOwnCount}</span>
                </div>
                <div className="flex flex-col gap-1 p-2 rounded-lg grow bg-shade_2">
                   <span className="text-sm text-primary">참여중인 TeamSpace</span>
-                  <span className="text-black dark:text-white">{profileInfo.teamSpaceCount}</span>
+                  <span className="text-black dark:text-white">{profileInfo?.teamSpaceCount}</span>
                </div>
             </div>
          </div>
