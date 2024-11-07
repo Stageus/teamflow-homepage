35f085f56d99f6da9ebc8bc3087f0cfdcb8dcd85
junik <wkdwnsdlr37@gmail.com>
2024-11-08 04:28:10 +0900

    Refactor(entities): useGetUsers 주석및 useEffect 추가

--- src/entities/useGetUsers.ts
+++ src/entities/useGetUsers.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface ProfileInfoObject {
    userIdx: number;
@@ -10,15 +10,23 @@ interface ProfileInfoObject {
 };
 type GetProfileInfoObject = ProfileInfoObject | null;
 
+/**
+ * @returns **profileInfo:** 요청이후 사용자 정보를 담은 data입니다.
+ * @example - const { profileInfo } = useGetUsers();
+*/
 export const useGetUsers = () => {
-   const [profileInfo, setProfileInfo] = useState<GetProfileInfoObject>({
-      userIdx: 1,
-      nickname: '테스트유저 닉네임',
-      email: 'userEmail@gmail.com',
-      profileImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZR6Gs7vDfFNNMsCAc2pNG0LaG3xAgnZDapQ&s',
-      teamSpaceOwnCount: 250, // 내가 생성한 teamspace 개수
-      teamSpaceCount: 240, // 내가 참여 중인 teamspace 개수
-   });
+   const [profileInfo, setProfileInfo] = useState<GetProfileInfoObject>(null);
+
+   useEffect(()=>{
+      setProfileInfo({
+         userIdx: 1,
+         nickname: '테스트유저 닉네임',
+         email: 'userEmail@gmail.com',
+         profileImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZR6Gs7vDfFNNMsCAc2pNG0LaG3xAgnZDapQ&s',
+         teamSpaceOwnCount: 250, // 내가 생성한 teamspace 개수
+         teamSpaceCount: 240, // 내가 참여 중인 teamspace 개수
+      })
+   },[])
 
    return { profileInfo };
 };
