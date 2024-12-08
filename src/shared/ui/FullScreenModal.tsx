32ad9ee6fecda86aad176a8d86d29d9a314abcb5
junik <wkdwnsdlr37@gmail.com>
2024-12-08 16:41:55 +0900

    Design(shared): FullScreenModal type 지정

--- src/shared/ui/FullScreenModal.tsx
+++ src/shared/ui/FullScreenModal.tsx
@@ -1,28 +1,44 @@
+// package
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-// import { BsXLg } from 'react-icons/bs';
-import { FullScreenModalProps } from "./types";
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Cross1Icon } from '@radix-ui/react-icons';
+// layer
+import { cn } from '@/shared/lib/cn';
 
-export const FullScreenModal = (props: FullScreenModalProps) => {
-   const { title, children, closeModal} = props;
-   
-   const modalStyles = {
-      show: 'visible translate-x-0 opacity-100',
-      hide: 'invisible -translate-x-full opacity-0',
-   };
+const fullScreenVariants = cva('modal-gradient fixed inset-0 flex transition-transform z-50', {
+   variants: {
+      variant: {
+         show: 'visible translate-x-0 opacity-100',
+         hide: 'invisible -translate-x-full opacity-0',
+      },
+   },
+   defaultVariants: {
+      variant: 'hide',
+   },
+});
+
+interface FullScreenModalProps extends VariantProps<typeof fullScreenVariants> {
+   title: string;
+   children: React.ReactElement;
+   closeModal: () => void;
+}
+
+const FullScreenModal = (props: FullScreenModalProps) => {
+   const { title, children, closeModal, variant } = props;
 
    return ReactDOM.createPortal(
-      <div
-         className={`modal-gradient fixed inset-0 flex transition-transform ${children ? modalStyles.show : modalStyles.hide} z-50`}>
-         <h1 className="absolute text-white flex justify-center p-5 text-[30px] italic">{title}</h1>
+      <div className={cn(fullScreenVariants({ variant }))}>
+         <h1 className="absolute flex justify-center p-5 text-[30px] italic text-white">{title}</h1>
          <div
             onClick={closeModal}
-            className="absolute flex items-center justify-end w-6 h-6 text-white cursor-pointer hover:text-secondary right-5 top-5">
-            {/* <BsXLg size={'100%'} /> */}
+            className="absolute flex items-center justify-end w-6 h-6 text-white cursor-pointer right-5 top-5 hover:text-secondary">
+            <Cross1Icon />
          </div>
-         <div className="flex items-center justify-center grow">
-         {children ?? null}
-         </div>
+         <div className="flex items-center justify-center grow">{children}</div>
       </div>,
       document.body,
    );
 };
+
+export { FullScreenModal };
