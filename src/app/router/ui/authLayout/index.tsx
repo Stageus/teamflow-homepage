c3865a89030326fdc6ac3f3109b0929cfa8bf270
junik <wkdwnsdlr37@gmail.com>
2024-12-08 18:41:46 +0900

    Design(app): aside container 적용

--- src/app/router/ui/authLayout/index.tsx
+++ src/app/router/ui/authLayout/index.tsx
@@ -1,18 +1,19 @@
 import { Outlet } from 'react-router-dom';
-import { Aside } from "@/widgets/aside";
-import { Header } from "@/widgets/header";
+import { Aside } from '@/widgets/aside';
+import { Header } from '@/widgets/header';
 
 export const AuthLayout = () => {
-
    return (
       <div className="flex h-full">
          <aside
-            className={`absolute -translate-x-full z-50 h-full w-[360px] md:relative md:translate-x-0 transition-transform`}>
-                <Aside/>
-            </aside>
+            className={`md:relative md:translate-x-0 w-full absolute z-50 h-full -translate-x-full transition-transform md:w-fit`}>
+            <div className='w-[360px]'>
+               <Aside />
+            </div>
+         </aside>
          <div className="flex flex-col grow">
             <header className="flex items-center justify-start bg-transparent">
-                <Header/>
+               <Header />
             </header>
             <main className="relative z-0 overflow-scroll grow">
                <Outlet />
