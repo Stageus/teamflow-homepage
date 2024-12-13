5136523469430f97133571ef373dbc6c7e973e1e
junik <wkdwnsdlr37@gmail.com>
2024-12-13 13:38:04 +0900

    Feat(widgets): header aside 이벤트추가

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -12,7 +12,8 @@ import { PATHS } from '@/shared/consts/paths';
 import { Button } from '@/shared/ui/Button';
 import { FullScreenModal } from '@/shared/ui/FullScreenModal';
 
-export const Header = () => {
+export const Header = ( props: {toggleTestAside: () => void} ) => {
+   const {toggleTestAside} = props;
    const currentUrl = useMatch(`/${PATHS.teamSpaceList}`);
    const isSameUrl = currentUrl.pathname === `/${PATHS.teamSpaceList}`;
 
@@ -21,11 +22,16 @@ export const Header = () => {
    const { alarmRef, isAlarmModal, toggleAlarmModal } = useAlarm();
    const { isLogoutModal, toggleLogoutModal, handelrLogout } = useLogout();
 
+
+
    return (
       <div className="flex gap-3 px-4 py-2 whitespace-nowrap">
          {/* 모바일 화면 햄버거 버튼 */}
-         <div className="flex items-center mx-3 hover:theme-hover-text text-gray md:hidden">
-            <HamburgerMenuIcon className="h-7 w-7"/>
+         <div
+            className="flex items-center mx-3 cursor-pointer hover:theme-hover-text md:hidden text-gray"
+            // onClick={toggleAside}>
+            onClick={toggleTestAside}>
+            <HamburgerMenuIcon className="h-7 w-7" />
          </div>
          {/* Teamspace 이동 버튼 */}
          <Button variant={isSameUrl ? 'select' : 'default'} onClick={teamspaceListRoute}>
@@ -46,20 +52,20 @@ export const Header = () => {
          <Button variant="default" onClick={toggleLogoutModal}>
             로그아웃
          </Button>
-         
+
          {/* 로그아웃 모달 */}
-         <FullScreenModal title="logout" closeModal={toggleLogoutModal} variant={ isLogoutModal ? 'show' : 'hide'}>
-               <div className="flex flex-col gap-8">
-                  <h2 className="text-white">로그아웃 하시겠습니까?</h2>
-                  <div className="flex gap-5">
-                     <Button variant="danger" onClick={handelrLogout}>
-                        로그아웃
-                     </Button>
-                     <Button variant="default" onClick={toggleLogoutModal}>
-                        되돌아가기
-                     </Button>
-                  </div>
+         <FullScreenModal title="logout" closeModal={toggleLogoutModal} variant={isLogoutModal ? 'show' : 'hide'}>
+            <div className="flex flex-col gap-8">
+               <h2 className="text-white">로그아웃 하시겠습니까?</h2>
+               <div className="flex gap-5">
+                  <Button variant="danger" onClick={handelrLogout}>
+                     로그아웃
+                  </Button>
+                  <Button variant="default" onClick={toggleLogoutModal}>
+                     되돌아가기
+                  </Button>
                </div>
+            </div>
          </FullScreenModal>
       </div>
    );
