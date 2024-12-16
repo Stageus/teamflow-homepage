49dcce13dda03a41bb80252929988d9756eda0c4
junik <wkdwnsdlr37@gmail.com>
2024-12-16 15:47:15 +0900

    Refactor(shared): useValidation 주석 설명 변경

--- src/shared/hooks/useValidation.ts
+++ src/shared/hooks/useValidation.ts
@@ -5,7 +5,7 @@ import { useState, useRef } from 'react';
  * @returns**inputRef:** 참고할 input의 Ref 입니다.
  * @returns**isValid:** 정규표현식의 통과 여부를 반환하는 boolean입니다.
  * @returns**isCheckValidation:** input value가 정규표현식여부를 통과를 체크하는 함수입니다.
- * @example - { inputRef, isValid, isCheckValidation } = useValidation(RegExp)
+ * @example - { inputRef, validationResult, isCheckValidation } = useValidation(RegExp)
  * @example - <input ref={inputRef} onEvent={isCheckValidation}/>..
  */
 
