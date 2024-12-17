c9bb6cac2be694743b47b381a7f5b4958b2d4c18
junik <wkdwnsdlr37@gmail.com>
2024-12-17 17:19:23 +0900

    Refactor(aside): userProfile useToggle 적용

--- src/widgets/aside/ui/userProfile/index.tsx
+++ src/widgets/aside/ui/userProfile/index.tsx
@@ -1,7 +1,6 @@
 // npm
 import { Cross1Icon, GearIcon } from '@radix-ui/react-icons';
 // slice
-import { useToogle } from './model/useToogle';
 import { useProfileImg } from './model/useProfileImg';
 // layer
 import { useGetUsers } from '@/entities/useGetUsers';
@@ -12,12 +11,18 @@ import { nickNameRegExp } from '@/shared/consts/regExp';
 import { cn } from '@/shared/lib/cn';
 import { Input } from '@/shared/ui/Input';
 import { Button } from '@/shared/ui/Button';
+import { useToggle } from '@/shared/hooks/useToggle';
 
 export const UserProfile = () => {
    // ui
-   const { inputRef, isValid, isCheckValidation } = useValidation(nickNameRegExp);
-   const { toggleState, changeToggleState } = useToogle(inputRef);
+   const { inputRef: nickNameRef, validationResult, isCheckValidation } = useValidation(nickNameRegExp);
    const { profileImgRef, profileImg, selectFile, cancelFile } = useProfileImg();
+   const [isMoreInfoOpen, setIsMoreInfoOpen] = useToggle(false);
+   const [isNickNameInputOpen, setIsNickNameInputOpen] = useToggle(false);
+   const allClose = () => {
+      if ( isNickNameInputOpen ) setIsNickNameInputOpen();
+      setIsMoreInfoOpen(); 
+   }
    // api
    const { profileInfo } = useGetUsers();
    const { putUsersProfileImg } = usePutUsersProfileImg();
@@ -36,31 +41,31 @@ export const UserProfile = () => {
                   <span className="text-shade_5">{profileInfo?.email}</span>
                   <div
                      className={cn('h-4 duration-200', {
-                        'h-24': toggleState.isNickNameInput && toggleState.isMoreInfo,
+                        'h-24': isNickNameInputOpen && isMoreInfoOpen,
                      })}>
-                     {toggleState.isNickNameInput && toggleState.isMoreInfo ? (
+                     {isNickNameInputOpen && isMoreInfoOpen ? (
                         <div className="flex flex-col gap-1">
                            <Input
                               type="text"
-                              ref={inputRef}
+                              ref={nickNameRef}
                               onChange={isCheckValidation}
                               defaultValue={profileInfo?.nickname}
                               placeholder="3글자 이상 ~ 10글자 이하"
                               className={cn(
-                                 { 'border-success': inputRef.current?.value && isValid },
-                                 { 'border-error': inputRef.current?.value && !isValid },
+                                 { 'border-success': validationResult === true },
+                                 { 'border-error': validationResult === false },
                               )}
                            />
-                           {inputRef.current?.value && (
-                              <p className={`text-xs ${isValid ? 'text-success' : 'text-error'}`}>
-                                 {isValid ? '사용가능합니다' : '3글자 이상 ~ 10글자 이하'}
+                           {nickNameRef.current?.value && (
+                              <p className={`text-xs ${validationResult ? 'text-success' : 'text-error'}`}>
+                                 {validationResult ? '사용가능합니다' : '3글자 이상 ~ 10글자 이하'}
                               </p>
                            )}
                            <div className="flex justify-start gap-3 py-2">
-                              <Button variant={isValid ? 'select' : 'disabeld'} sizes="sm">
+                              <Button variant={validationResult ? 'select' : 'disabeld'} sizes="sm">
                                  닉네임 저장
                               </Button>
-                              <Button sizes="sm" onClick={() => changeToggleState('isNickNameInput')}>
+                              <Button sizes="sm" onClick={setIsNickNameInputOpen}>
                                  취소
                               </Button>
                            </div>
@@ -70,7 +75,7 @@ export const UserProfile = () => {
                         <span
                            className={cn('visible max-h-20 py-2 text-primary opacity-100 transition-all duration-200', {
                               'invisible max-h-0 opacity-0 duration-75':
-                                 toggleState.isNickNameInput && toggleState.isMoreInfo,
+                                 isNickNameInputOpen && isMoreInfoOpen,
                            })}>
                            {profileInfo?.nickname}
                         </span>
@@ -79,7 +84,7 @@ export const UserProfile = () => {
                   <div
                      className={cn('invisible flex max-h-0 gap-2 opacity-0 transition-all duration-75', {
                         'visible max-h-20 py-2 opacity-100 duration-200':
-                           toggleState.isMoreInfo && !toggleState.isNickNameInput,
+                           isMoreInfoOpen && !isNickNameInputOpen,
                      })}>
                      {profileImg.fileObj && profileImg.preview ? (
                         <>
@@ -104,7 +109,7 @@ export const UserProfile = () => {
                               </div>
                            </Button>
                            {/* 이름 변경 버튼 */}
-                           <Button variant="select" sizes="sm" onClick={() => changeToggleState('isNickNameInput')}>
+                           <Button variant="select" sizes="sm" onClick={setIsNickNameInputOpen}>
                               닉네임 변경
                            </Button>
                         </>
@@ -113,9 +118,9 @@ export const UserProfile = () => {
                </div>
                {/* 유저정보 더보기 버튼*/}
                <div
-                  onClick={() => changeToggleState('isMoreInfo')}
+                  onClick={allClose}
                   className="flex items-center justify-end w-5 h-5 text-black cursor-pointer dark:text-white">
-                  {toggleState.isMoreInfo ? (
+                  {isMoreInfoOpen ? (
                      <Cross1Icon className={'h-full w-full'} />
                   ) : (
                      <GearIcon className={'h-full w-full'} />
@@ -127,9 +132,12 @@ export const UserProfile = () => {
 
             {/* 유저 TeamSpace정보 영역*/}
             <div
-               className={cn('invisible flex max-h-0 gap-3 opacity-0 transition-all duration-75 text-black dark:text-white', {
-                  'visible max-h-20 py-2 opacity-100 duration-200': toggleState.isMoreInfo,
-               })}>
+               className={cn(
+                  'invisible flex max-h-0 gap-3 text-black opacity-0 transition-all duration-75 dark:text-white',
+                  {
+                     'visible max-h-20 py-2 opacity-100 duration-200': isMoreInfoOpen,
+                  },
+               )}>
                <div className="flex flex-col gap-1 p-2 rounded-lg grow bg-shade_2">
                   <span className="text-sm text-primary">나의 TeamSpace</span>
                   <span>{profileInfo?.teamSpaceOwnCount}</span>
