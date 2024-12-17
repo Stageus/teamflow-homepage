4ff9b0bc30ad68a07ec212dd2ce1c62856e42384
junik <wkdwnsdlr37@gmail.com>
2024-12-17 17:17:48 +0900

    file: 불필요한 파일삭제

--- src/widgets/aside/ui/teamSpaceChannelList/model/useChannelList.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-export const useChannelList = () => {
-   const [channelList, setChannelList] = useState(false);
-   const toogleChannelList = () => setChannelList(!channelList);
-
-   return { channelList, toogleChannelList };
-};
--- src/widgets/aside/ui/teamSpaceChannelList/model/useCreateChannel.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export const useCreateChannel = () => {
-   const [createChannelModal, setCreateChannelModal] = useState(false);
-   const toogleCreateChannelModal = () => setCreateChannelModal(!createChannelModal);
-
-   return { createChannelModal, toogleCreateChannelModal };
-};
