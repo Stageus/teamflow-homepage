8e6a8141abcb754967bbd5c5b41751904de0b40e
junik <wkdwnsdlr37@gmail.com>
2024-12-21 17:24:52 +0900

    Refactor(shared): useCookie 타입 설정

--- src/shared/hooks/useCookie.ts
+++ src/shared/hooks/useCookie.ts
@@ -1,11 +1,11 @@
 import { useCookies } from 'react-cookie';
 
-export const useCookie = (dependenciesCookie: string) => {
+export const useCookie = (dependenciesCookie: string): UseCookieReturnType => {
    const [cookies, setCookie, removeCookie] = useCookies([dependenciesCookie], {
       // doNotParse: "boolean" -> 가져온 cookie값에 대한 객체 변환유무
       // doNotUpdate: "boolean" -> 컴포넌트 마운트될때 쿠키 업데이트 유무
    });
-   const setCookieName = (name: string, value: string, period: Date) => {
+   const setCookieName = (name: string, value: string, period: Date | null) => {
       const express = period ? { expires: period } : {};
       return setCookie(name, value, { path: '/', ...express });
    };
@@ -16,3 +16,9 @@ export const useCookie = (dependenciesCookie: string) => {
 
    return { cookies, setCookieName, removeCookieName };
 };
+
+type UseCookieReturnType = {
+   cookies: { [key: string]: string };
+   setCookieName: (name: string, value: string, period: Date | null) => void;
+   removeCookieName: (name: string) => void;
+} & {};
