79bc7ccf3cf8d4418a37fa9e19f98528d9a053e3
junik <wkdwnsdlr37@gmail.com>
2024-12-21 18:50:07 +0900

    Design(widgets): PrivateChannelModal Button적용

--- src/widgets/aside/ui/teamSpaceChannelList/ui/createPrivateChannelModal/index.tsx
+++ src/widgets/aside/ui/teamSpaceChannelList/ui/createPrivateChannelModal/index.tsx
@@ -2,6 +2,7 @@ import { useValidation } from '@/shared/hooks/useValidation';
 import { Input } from '@/shared/ui/Input';
 import { channelRegexp } from '@/shared/consts/regExp';
 import { cn } from '@/shared/lib/cn';
+import { Button } from '@/shared/ui/Button';
 
 const CreatePrivateChannelModal = ( props: CreatePrivateChannelModalProps ) => {
    const { inputRef: privateChannelRef, validationResult, isCheckValidation } = useValidation(channelRegexp);
@@ -22,18 +23,8 @@ const CreatePrivateChannelModal = ( props: CreatePrivateChannelModalProps ) => {
             {validationResult !== null && (validationResult ? '사용가능합니다' : '5글자 이상 ~ 20글자 이하')}
          </p>
          <div className="flex justify-center gap-3 py-2">
-            <div>
-               <button
-                  className={`button-layout ${validationResult ? '_active' : '_disabled'}`}
-                  onClick={() => console.log(privateChannelRef.current?.value)}>
-                  채널생성
-               </button>
-            </div>
-            <div>
-               <button className="button-layout _default" onClick={props.closeModal}>
-                  취소
-               </button>
-            </div>
+            <Button variant={validationResult ? 'select' : 'disabeld'} onClick={()=> validationResult && console.log(privateChannelRef.current?.value)}>채널생성</Button>
+            <Button variant='default' onClick={props.closeModal}>취소</Button>
          </div>
       </div>
    );
