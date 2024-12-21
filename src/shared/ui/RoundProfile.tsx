1f29e04a7407b96f531f5d81de513d3e9cf8dd54
junik <wkdwnsdlr37@gmail.com>
2024-12-21 16:57:22 +0900

    Refactor(shared): RoundProfile 컴포넌트 type 분리

--- src/shared/ui/RoundProfile.tsx
+++ src/shared/ui/RoundProfile.tsx
@@ -1,6 +1,10 @@
 import userProfile from '../assets/userProfile.svg';
-export const RoundProfile = (props: { imgUrl?: string }) => {
+export const RoundProfile = (props: RoundProfileProps) => {
    const { imgUrl } = props;
 
    return <img src={imgUrl ?? userProfile} className="w-full h-full rounded-full" />;
 };
+
+type RoundProfileProps = {
+   imgUrl?: string;
+}
