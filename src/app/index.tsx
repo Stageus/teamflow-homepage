cb8416a6423c09cb1e79eb04661c735747923b84
junik <wkdwnsdlr37@gmail.com>
2024-12-23 17:07:34 +0900

    Feat(app): PrivatechannelContext 적용

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,15 @@ import { RouterProvider } from 'react-router-dom';
 // slice
 import './global.css';
 import { Router } from './router/Router';
+// layer
+import { PrivateChannelProvider } from '@/shared/context/privateChannelContext';
 
 
 function App() {
    return (
-      <>
+      <PrivateChannelProvider>
          <RouterProvider router={Router} />
-      </>
+      </PrivateChannelProvider>
    );
 }
 
