3922268606dff2abda9b0f236a4492994defc06e
junik <wkdwnsdlr37@gmail.com>
2024-12-30 16:58:36 +0900

    Feat(app): ModalProvider 적용

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,15 @@ import './global.css';
 import { Router } from './router/Router';
 // layer
 import { PrivateChannelProvider } from '@/shared/context/privateChannelContext';
-
+import { ModalProvider } from '@/shared/context/modalContext';
 
 function App() {
    return (
-      <PrivateChannelProvider>
-         <RouterProvider router={Router} />
-      </PrivateChannelProvider>
+      <ModalProvider>
+         <PrivateChannelProvider>
+            <RouterProvider router={Router} />
+         </PrivateChannelProvider>
+      </ModalProvider>
    );
 }
 
